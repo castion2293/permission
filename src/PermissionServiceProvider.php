8f0f2f9c74ea466ad779a210c55d96c8306a5a1f
Nick Zhang <castion2293@yahoo.com.tw>
2021-04-19 15:24:47 +0800

    feat: 初始專案(@nick)

---
+++ src/PermissionServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Pharaoh\Permission;
+
+use Illuminate\Support\ServiceProvider;
+
+class PermissionServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+        parent::register();
+    }
+}
---
+++ tests/ExampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Pharaoh\Permission\Tests;
+
+class ExampleTest extends BaseTestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
